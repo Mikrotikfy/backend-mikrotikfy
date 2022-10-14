248ab06fa36d3c62fd0d3382fa7bb1819c1ec93f
Nicolas Echeverry <nikoxx9900@gmail.com>
2022-10-14 16:20:45 -0500

    Notifiction webhook v2

--- src/api/webhook/controllers/webhook.js
+++ src/api/webhook/controllers/webhook.js
@@ -1,5 +1,5 @@
 'use strict';
-
+const fetch = require('node-fetch');
 /**
  * A set of functions called "actions" for `webhook`
  */
@@ -25,38 +25,32 @@ module.exports = {
     const TOKEN = process.env.TOKEN
 
     const data = ctx.request.body
-    console.log(data.object)
-    if (data.object) {
-      if (
-        data.entry &&
-        data.entry[0].changes &&
-        data.entry[0].changes[0] &&
-        data.entry[0].changes[0].value.messages &&
-        data.entry[0].changes[0].value.messages[0]
-      ) {
-        const phone_number_id = data.entry[0].changes[0].value.metadata.phone_number_id
-        const from = data.entry[0].changes[0].value.messages[0].from
-        const msg_body = data.entry[0].changes[0].value.messages[0].text.body
-        console.log(phone_number_id, from, msg_body)
-        fetch(`https://graph.facebook.com/v14.0/${phone_number_id}/messages?access_token=${TOKEN}`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            data: {
-              messaging_product: 'whatsapp',
-              to: from,
-              text: {
-                body: `Akc: ${msg_body}`
-              }
+    console.log(data)
+    if (
+      data.value &&
+      data.value.messages &&
+      data.value.messages[0]
+    ) {
+      const phone_number_id = data.value.metadata.phone_number_id
+      const from = data.value.messages[0].from
+      const msg_body = data.value.messages[0].text.body
+      console.log(phone_number_id, from, msg_body)
+      fetch(`https://graph.facebook.com/v14.0/${phone_number_id}/messages?access_token=${TOKEN}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          data: {
+            messaging_product: 'whatsapp',
+            to: from,
+            text: {
+              body: `Akc: ${msg_body}`
             }
-          })
+          }
         })
-        return 200
-      } else {
-        return 403
-      }
+      })
+      return 200
     } else {
       return 403
     }
