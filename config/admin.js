c1c66782de063e83c6738326ec582faa623fff49
Nicolas Echeverry <nikoxx9900@gmail.com>
2023-02-09 18:41:17 -0500

    refactor(backend): :ambulance: Deprecated client fields

--- config/admin.js
+++ config/admin.js
@@ -3,6 +3,6 @@ module.exports = ({ env }) => ({
     secret: env('ADMIN_JWT_SECRET', 'bfabe71a0d186651826395c050b9df58'),
   },
   apiToken: {
-    salt: env('ADMIN_JWT_SECRET')
+    salt: '322a07aa2e990496c612d15cfe0be74e'
   }
 });
--- src/api/client/controllers/searchclient.js
+++ src/api/client/controllers/searchclient.js
@@ -71,7 +71,7 @@ module.exports = {
                 }
               ],  
             },
-            populate: ['city', 'plan', 'addresses', 'addresses.neighborhood', 'neighborhood', 'technology', 'clienttype', 'vlan', 'offer', 'offer.plan', 'offermovements.offer', 'monthlybills', 'tvspec', 'tvspec.tvspectype' ],
+            populate: ['city', 'plan', 'addresses', 'addresses.neighborhood', 'neighborhood', 'technology', 'clienttype', 'vlan', 'offer', 'offer.plan', 'offermovements.offer', 'offermovements', 'debtmovements', 'debtmovements.technician', 'monthlybills', 'tvspec', 'tvspec.tvspectype' ],
             orderBy: { code: 'asc' }
           })
           const sanitizedEntity = await sanitize.contentAPI.output(res);
