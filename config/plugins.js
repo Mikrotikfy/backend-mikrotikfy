9c6a9aa4540338ea10ccd8ecefe849933695e8f4
Nicolas Echeverry <nikoxx9900@gmail.com>
2023-04-21 16:35:53 -0500

    feat(backend): :sparkles: Schema for optical power on client

--- config/plugins.js
+++ config/plugins.js
@@ -12,11 +12,11 @@ module.exports = ({ env }) => ({
   },
   upload: {
     config: {
+      sizeLimit: 250 * 1024 * 1024, // 256mb in bytes
       providerOptions: {
         localServer: {
           maxage: 300000
         },
-        sizeLimit: 250 * 1024 * 1024 // 256mb in bytes
       },
       formLimit: "256mb", // modify form body
       jsonLimit: "256mb", // modify JSON body
