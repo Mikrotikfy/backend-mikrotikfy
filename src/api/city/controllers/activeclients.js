3b4c88533963b3a71e2164919f31a761b62de051
Nicolas Echeverry <nikoxx9900@gmail.com>
2023-08-28 11:11:14 -0500

    Send active clients update

--- src/api/city/controllers/activeclients.js
+++ src/api/city/controllers/activeclients.js
@@ -22,15 +22,77 @@ module.exports = {
         conn.close()
         cityActiveClients.push(result)
       }
-      return cityActiveClients[0].concat(cityActiveClients[1])
+      await sendWhatsApp(id, result2.length)
+      await updateCityActive(id, cityActiveClients[0].concat(cityActiveClients[1]).length)
+      return {
+        activeclients: cityActiveClients[0].concat(cityActiveClients[1]).length
+      }
     } else {
-      const conn = APIARNOP(`${ip}:${port}`)
+      const conn = await APIARNOP(`${ip}:${port}`)
       await conn.connect()
       const result2 = await conn.write('/ppp/active/print', [
         '=.proplist=name',
       ])
       conn.close()
-      return result2
+      await sendWhatsApp(id, result2.length)
+      await updateCityActive(id, result2.length)
+      return {
+        activeclients: result2.length
+      }
     }
   },
 };
+
+function updateCityActive (id, activeClients) {
+  return new Promise((resolve, reject) => {
+    const cityQuery = strapi.service('api::city.city').update(id, { data: { active: activeClients+'' } });
+    resolve(cityQuery)
+  })
+}
+
+function sendWhatsApp (id, activeClients) {
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`
+    },
+    body: JSON.stringify(
+      {
+        messaging_product: 'whatsapp',
+        to: `573125090684`,
+        type: 'template',
+        template: {
+          name: 'sysadmin_active_count',
+          language: {
+            code: 'es'
+          },
+          components: [
+            {
+              type: 'body',
+              parameters: [
+                {
+                  type: 'text',
+                  text: `${activeClients > 0 ? 'ONLINE' : 'OFFLINE' }`
+                },
+                {
+                  type: 'text',
+                  text: `${activeClients}`
+                }
+              ]
+            }
+          ]
+        }
+      }
+    )
+  }
+  return new Promise((resolve, reject) => {
+    fetch('https://graph.facebook.com/v15.0/100480202798133/messages', options)
+      .then(function (response) {
+        return resolve(response)
+      })
+      .catch(function (err) {
+        return reject(err)
+      })
+  })
+}
