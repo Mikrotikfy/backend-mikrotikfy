bd773e80412cc099ce1d14caf79971027112c87c
Nicolas Echeverry <nikoxx9900@gmail.com>
2023-12-04 14:31:26 -0500

    fix pppoe log

--- src/api/client/controllers/pppoeregister.js
+++ src/api/client/controllers/pppoeregister.js
@@ -9,22 +9,22 @@ module.exports = {
     const type = ctx.request.query.type
     const city = ctx.request.query.city
 
-    const search = await strapi.service('api::client.client').find({ filters: { code: client, city: { name: city } }})
+    const search = await strapi.service('api::service.service').find({ filters: { code: client, city: { name: city } }})
     if (search.results.length > 0) {
       await strapi.service('api::pppoe-event.pppoe-event').create({
         data: {
           type,
-          client: search.results[0].id
+          service: search.results[0].id
         }
       })
       return true
     } else {
-      const searchdni = await strapi.service('api::client.client').find({ filters: { dni: client, city: { name: city } }})
+      const searchdni = await strapi.service('api::service.service').find({ filters: { dni: client, city: { name: city } }})
       if (searchdni.results.length > 0) {
         await strapi.service('api::pppoe-event.pppoe-event').create({
           data: {
             type,
-            client: searchdni.results[0].id
+            service: searchdni.results[0].id
           }
         })
         return true
